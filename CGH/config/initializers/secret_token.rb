17213955a6dd77279ab9ea25b9bf1a4add59ae1a
Albin ALBRIQUE <aalbrique@gmail.com>
2012-08-26 16:06:25 +0200

    Commit initial
    Premier commit de l'applicatif CGH - Console de Gestion des Heures

---
+++ CGH/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CGH::Application.config.secret_token = 'f5954b956a325d7d5afee25c860d8d49549e60d84391fd23857bec03a77861898296eb1a91f383571fa99f085c86b8431ddfafdd843ff0cdb1d63d50b6a60889'
